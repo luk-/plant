efd1c02e8d0b97c485c148457084251b19011e21
Luke Arduini <luke.arduini@gmail.com>
2013-03-02 07:09:29 -0500

    properly emitting data events

--- index.js
+++ index.js
@@ -24,13 +24,11 @@ module.exports = function (data, html) {
   if (!html) {
     var stream = new Stream
     stream.writable = stream.readable = true
-    var htmlBuffer = ''
     stream.write = function (buf) {
-      htmlBuffer += buf.toString()
+      this.emit('data', plant(data, buf.toString()))
     }
     stream.end = function () {
       stream.writable = false
-      this.emit('data', plant(data, htmlBuffer))
     }
     return stream
   }
